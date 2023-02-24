64755d6b6b74d9bf071878aba6b1137d35c4ff0d
harshit <harshitgosami103@gmail.com>
2023-02-24 23:03:45 +0530

    our courses dialog done

--- app/src/main/java/com/harshit/goswami/collegeapp/student/HomeFragment.kt
+++ app/src/main/java/com/harshit/goswami/collegeapp/student/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.harshit.goswami.collegeapp.student
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
@@ -15,6 +18,7 @@ import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
 import com.google.android.material.appbar.MaterialToolbar
 import com.harshit.goswami.collegeapp.R
+import com.harshit.goswami.collegeapp.databinding.DialogOurCoursesDetailsBinding
 import com.harshit.goswami.collegeapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,6 +26,7 @@ class HomeFragment : Fragment() {
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var toolbar: MaterialToolbar
     private lateinit var drawerLayout: DrawerLayout
+
     // ********************** admission guidelines variables ***************
     private var isAdmissionNotice = false
     private var isAdmissionGuidelines = false
@@ -50,8 +55,10 @@ class HomeFragment : Fragment() {
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 //########################################################################################################
+
         imageSlider()
         admissionGuidelinesSetUp()
+        ourCoursesSetUp()
         binding.scrollView3.viewTreeObserver.addOnScrollChangedListener {
             val y = binding.scrollView3.scrollY
             if (y > 500) {
@@ -66,7 +73,6 @@ class HomeFragment : Fragment() {
         binding.FHInstaIcon.setOnClickListener { getByUrl("https://www.instagram.com/chetanas_sfc/?hl=en") }
         binding.FHFacebookIcon.setOnClickListener { getByUrl("https://www.facebook.com/profile.php?id=100064103347725") }
         binding.FHEmailIcon.setOnClickListener {}
-
 
 
 //        binding.navView.setNavigationItemSelectedListener {
@@ -174,20 +180,88 @@ class HomeFragment : Fragment() {
 
     }
 
+    private fun ourCoursesSetUp() {
+        binding.FHCardBscit.setOnClickListener {
+            val courseBinding =
+                DialogOurCoursesDetailsBinding.inflate(layoutInflater)
+            val dialog = Dialog(requireContext(), R.style.BottomSheetStyle)
+            dialog.setContentView(courseBinding.root)
+           dialog.show()
+        }
+        binding.FHCardBaf.setOnClickListener {
+            val courseBinding =
+                DialogOurCoursesDetailsBinding.inflate(layoutInflater)
+            val dialog = Dialog(requireContext(), R.style.BottomSheetStyle)
+            dialog.setContentView(courseBinding.root)
+//            dialog
+//                .setCanceledOnTouchOutside(true)
+//            dialog.window?.setGravity(Gravity.CENTER)
+//            dialog.setCancelable(true)
+//            builder.setCanceledOnTouchOutside(true)
+
+            courseBinding.txtDepartment.text = "BAF"
+            courseBinding.txtCourseDescription.text =
+                "The Bachelor of Accounting and Finance (BAF) course provides comprehensive training to students in the field of Accounting & Finance by way of interaction, projects, presentations, industrial visits, practical training, job orientation and placements."
+            courseBinding.txtEligibilityCriteria.text =
+                "\uF0D8\tPassed XII Std. Examination of the Maharashtra Board of Higher Secondary Education\n" +
+                        "\uF0D8\tNot have secured less than 45% marks."
+            courseBinding.txtDurationAndInfo.text =
+                "\uF0D8\tSix semester spread over three years.\n" +
+                        "\uF0D8\t40 modules comprising 39 theory papers and 01 projects.\n" +
+                        "\uF0D8\tLectures per course (subject) shall be a minimum of 50 each of 50 minutes duration."
+            dialog.show()
+        }
+        binding.FHCardBms.setOnClickListener {
+            val courseBinding =
+                DialogOurCoursesDetailsBinding.inflate(layoutInflater)
+            val dialog = Dialog(requireContext(), R.style.BottomSheetStyle)
+            dialog.setContentView(courseBinding.root)
+            courseBinding.txtDepartment.text = "BMS"
+            courseBinding.txtCourseDescription.text =
+                "The Bachelor of Management Studies (BMS) course has designed to create professionally qualified management executives, with effect and interpersonal communication in the field of management."
+            courseBinding.txtEligibilityCriteria.text =
+                "\uF0D8\tPassed XII Std. Examination of the Maharashtra Board of Higher Secondary Education\n" +
+                        "\uF0D8\tNot have secured less than 45% marks."
+            courseBinding.txtDurationAndInfo.text =
+                "\uF0D8\tSix semester spread over three years.\n" +
+                        "\uF0D8\t40 modules comprising 39 theory papers and 01 projects.\n" +
+                        "\uF0D8\tLectures per course (subject) shall be a minimum of 50 each of 50 minutes duration."
+            dialog.show()
+        }
+        binding.FHCardBammc.setOnClickListener {
+            val courseBinding =
+                DialogOurCoursesDetailsBinding.inflate(layoutInflater)
+            val dialog = Dialog(requireContext(), R.style.BottomSheetStyle)
+            dialog.setContentView(courseBinding.root)
+            courseBinding.txtDepartment.text = "BAMMC"
+            courseBinding.txtCourseDescription.text =
+                "The Bachelors in Mass Media (BMM) programme has been designed with this very agenda – to produce professionals armed with specialized skills in either advertising or journalism."
+            courseBinding.txtEligibilityCriteria.text =
+                "\uF0D8\tPassed XII Std. Examination of the Maharashtra Board of Higher Secondary Education\n" +
+                        "\uF0D8\tNot have secured less than 35% marks."
+            courseBinding.txtDurationAndInfo.text =
+                "\uF0D8\tSix semester spread over three years.\n" +
+                        "\uF0D8\tCourse contain 36 modules. \n" +
+                        "\uF0D8\tLectures per course (subject) shall be a minimum of 50 each of 50 minutes duration."
+            dialog.show()
+        }
+    }
+
     private fun getByUrl(s: String) {
         val uri = Uri.parse(s)
         startActivity(Intent(Intent.ACTION_VIEW, uri))
     }
-private fun imageSlider(){
-    val imageList = ArrayList<SlideModel>() // Create image list
 
-    imageList.add(SlideModel(R.drawable.slide1))
-    imageList.add(SlideModel(R.drawable.slide5))
-    imageList.add(SlideModel(R.drawable.slide6))
-    imageList.add(SlideModel(R.drawable.slide_gps_map_camera))
-    imageList.add(SlideModel(R.drawable.slide_best_college_certificate))
-    imageList.add(SlideModel(R.drawable.slide_college_iso_certificate))
+    private fun imageSlider() {
+        val imageList = ArrayList<SlideModel>() // Create image list
 
-    binding.imageSlider.setImageList(imageList, ScaleTypes.FIT)
-}
+        imageList.add(SlideModel(R.drawable.slide1))
+        imageList.add(SlideModel(R.drawable.slide5))
+        imageList.add(SlideModel(R.drawable.slide6))
+        imageList.add(SlideModel(R.drawable.slide_gps_map_camera))
+        imageList.add(SlideModel(R.drawable.slide_best_college_certificate))
+        imageList.add(SlideModel(R.drawable.slide_college_iso_certificate))
+
+        binding.imageSlider.setImageList(imageList, ScaleTypes.FIT)
+    }
 }
